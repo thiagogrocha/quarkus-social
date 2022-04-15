f5bbc479eda121615fa300e1dcc1fcd75816dd98
TRochaDev <trochadev@gmail.com>
2022-04-15 18:58:53 -0300

    Adiciona validations.

--- src/main/java/br/com/trochadev/quarkussocial/user/dto/UserDtoIn.java
+++ src/main/java/br/com/trochadev/quarkussocial/user/dto/UserDtoIn.java
@@ -1,7 +1,13 @@
 package br.com.trochadev.quarkussocial.user.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class UserDtoIn {
+
+    @NotBlank(message = "Campo nome é obrigatório!")
     private String name;
+    @NotNull(message = "Campo idade é obrigatório!")
     private Integer age;
 
     public String getName() {
