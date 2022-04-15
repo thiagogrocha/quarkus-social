c7515742f32675d60b60dbc497bd9f417fab2ad9
TRochaDev <trochadev@gmail.com>
2022-04-15 12:14:33 -0300

    Cria dto de Usuários.

---
+++ src/main/java/br/com/trochadev/quarkussocial/user/dto/UserDtoIn.java
@@ -0,0 +1,22 @@
+package br.com.trochadev.quarkussocial.user.dto;
+
+public class UserDtoIn {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
