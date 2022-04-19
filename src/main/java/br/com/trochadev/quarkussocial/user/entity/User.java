9bd290ab9c18be5b2088a72364dbf1c3ce1462c1
TRochaDev <trochadev@gmail.com>
2022-04-19 00:02:08 -0300

    Cria construtores.

--- src/main/java/br/com/trochadev/quarkussocial/user/entity/User.java
+++ src/main/java/br/com/trochadev/quarkussocial/user/entity/User.java
@@ -5,10 +5,10 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+@Data
 @Entity
 @Table(name = "users")
 @SequenceGenerator(name = "UsersSeq", sequenceName = "users_seq", allocationSize = 1)
-@Data
 public class User {
 
     @Id
@@ -21,4 +21,11 @@ public class User {
     @Column(nullable = false)
     private Integer age;
 
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 }
