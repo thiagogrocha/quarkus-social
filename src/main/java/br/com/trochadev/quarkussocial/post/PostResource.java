012789f32791a502ab62fd265be9250190d76920
TRochaDev <trochadev@gmail.com>
2022-04-27 10:47:16 -0300

    Cria classe responsável pelos serviços e adiciona informações dos endpoints utilizados no Swagger.

--- src/main/java/br/com/trochadev/quarkussocial/post/PostResource.java
+++ src/main/java/br/com/trochadev/quarkussocial/post/PostResource.java
@@ -1,134 +1,90 @@
 package br.com.trochadev.quarkussocial.post;
 
-import br.com.trochadev.quarkussocial.post.dto.PostDtoIn;
-import br.com.trochadev.quarkussocial.post.dto.PostDtoOut;
-import br.com.trochadev.quarkussocial.post.entity.Post;
-import br.com.trochadev.quarkussocial.post.entity.PostRepository;
-import br.com.trochadev.quarkussocial.user.entity.User;
-import br.com.trochadev.quarkussocial.user.entity.UserRepository;
 import br.com.trochadev.quarkussocial.validation.ResponseError;
-import io.quarkus.panache.common.Parameters;
-import io.quarkus.panache.common.Sort;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
-import javax.transaction.Transactional;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Path("/users/{userId}/posts")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
 
-    private UserRepository userRepo;
-    private PostRepository repo;
-    private Validator validator;
+    PostService service;
 
     @Inject
-    public PostResource(PostRepository repo, UserRepository userRepo, Validator validator) {
-        this.repo = repo;
-        this.userRepo = userRepo;
-        this.validator = validator;
+    public PostResource(PostService service) {
+        this.service = service;
     }
 
     @POST
-    @Transactional
+    @Operation(summary = "Cria Post", description = "Cria um Post")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "201", description = "Criado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = PostDtoOut.class))),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "403", description = "Usuário não encontrado"),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
     public Response savePost(@PathParam("userId") Long userId, PostDtoIn dto) {
         try {
-            Set<ConstraintViolation<PostDtoIn>> violations = validator.validate(dto);
-
-            if (!violations.isEmpty())
-                return ResponseError
-                        .createFromValidation(violations)
-                        .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
-
-            User user = userRepo.findById(userId);
-
-            if (user == null)
-                return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Usuário não encontrado!").build();
-
-            repo.persist(new Post(user, dto.getText()));
-
-            return Response.status(Response.Status.CREATED).build();
-
+            return service.save(userId, dto);
         } catch (Exception e) {
             e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Erro interno!\n" + e).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
         }
     }
 
     @GET
+    @Operation(summary = "Lista Posts", description = "Lista Posts de um Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = PostDtoOut.class))),
+            @APIResponse(responseCode = "403", description = "Usuário não encontrado"),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
     public Response listPosts(@PathParam("userId") Long userId) {
         try {
-            User user = userRepo.findById(userId);
-
-            if (user == null)
-                return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Usuário não encontrado!").build();
-
-            return Response.ok(repo.find("user", Sort.by("dateTime", Sort.Direction.Descending), user)
-                    .stream()
-                    .map(PostDtoOut::fromEntity)
-                    .collect(Collectors.toList())).build();
-
+            return service.posts(userId);
         } catch (Exception e) {
             e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Erro interno!\n" + e).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
         }
     }
 
     @PUT
     @Path("{postId}")
-    @Transactional
+    @Operation(summary = "Atualiza Post", description = "Atualiza um Post")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = PostDtoOut.class))),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "403", description = "Usuário ou Post não encontrado"),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
     public Response updatePost(@PathParam("userId") Long userId, @PathParam("postId") Long postId, PostDtoIn dto) {
         try {
-            Set<ConstraintViolation<PostDtoIn>> violations = validator.validate(dto);
-
-            if (!violations.isEmpty())
-                return ResponseError
-                        .createFromValidation(violations)
-                        .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
-
-            User user = userRepo.findById(userId);
-
-            if (user == null)
-                return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Usuário não encontrado!").build();
-
-            Post post = repo.find("user = :user and id = :id", Parameters.with("user", user).and("id", postId)).firstResult();
-            post.setText(dto.getText());
-
-            return Response.status(Response.Status.OK.getStatusCode(), "Post atualizado com sucesso!").build();
-
+            return service.update(userId, postId, dto);
         } catch (Exception e) {
             e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Erro interno!\n" + e).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
         }
     }
 
     @DELETE
     @Path("{postId}")
-    @Transactional
     public Response deletePost(@PathParam("userId") Long userId, @PathParam("postId") Long postId) {
         try {
-            User user = userRepo.findById(userId);
-            if (user == null)
-                return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Usuário não encontrado!").build();
-
-            Post post = repo.find("user = :user and id = :id", Parameters.with("user", user).and("id", postId)).firstResult();
-            if (post == null)
-                return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Post não encontrado!").build();
-
-            repo.delete(post);
-
-            return Response.status(Response.Status.NO_CONTENT.getStatusCode(), "Post deletado com sucesso!").build();
-
+            return service.delete(userId, postId);
         } catch (Exception e) {
             e.printStackTrace();
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Erro interno!\n" + e).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
         }
     }
 }
