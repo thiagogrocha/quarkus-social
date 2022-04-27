a692a602e90a2e8b284b1d9e5ad2acd0f4c02e3d
TRochaDev <trochadev@gmail.com>
2022-04-27 10:30:51 -0300

    Altera pacote.

--- src/main/java/br/com/trochadev/quarkussocial/validation/FieldError.java
+++ src/main/java/br/com/trochadev/quarkussocial/validation/FieldError.java
@@ -1,4 +1,4 @@
-package br.com.trochadev.quarkussocial.handler;
+package br.com.trochadev.quarkussocial.validation;
 
 public class FieldError {
     private String field;
--- src/main/java/br/com/trochadev/quarkussocial/validation/ResponseError.java
+++ src/main/java/br/com/trochadev/quarkussocial/validation/ResponseError.java
@@ -1,4 +1,4 @@
-package br.com.trochadev.quarkussocial.handler;
+package br.com.trochadev.quarkussocial.validation;
 
 import lombok.Data;
 
