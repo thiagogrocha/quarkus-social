8794731fa44baf15a564277166c3cfc141d37922
TRochaDev <trochadev@gmail.com>
2022-04-27 10:29:13 -0300

    Cria classe responsável pelos serviços de Post.

---
+++ src/main/java/br/com/trochadev/quarkussocial/post/PostService.java
@@ -0,0 +1,101 @@
+package br.com.trochadev.quarkussocial.post;
+
+import br.com.trochadev.quarkussocial.handler.ResponseMsg;
+import br.com.trochadev.quarkussocial.user.UserResource;
+import br.com.trochadev.quarkussocial.user.entity.User;
+import br.com.trochadev.quarkussocial.user.entity.UserRepository;
+import br.com.trochadev.quarkussocial.handler.ResponseError;
+import io.quarkus.panache.common.Parameters;
+import io.quarkus.panache.common.Sort;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Transactional
+@ApplicationScoped
+public class PostService {
+
+    private UserRepository userRepo;
+    private PostRepository repo;
+    private Validator validator;
+
+    @Inject
+    public PostService(PostRepository repo, UserRepository userRepo, Validator validator) {
+        this.repo = repo;
+        this.userRepo = userRepo;
+        this.validator = validator;
+    }
+
+    public Response save(Long userId, PostDtoIn dto) {
+        Set<ConstraintViolation<PostDtoIn>> violations = validator.validate(dto);
+
+        if (!violations.isEmpty())
+            return ResponseError
+                    .createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
+
+        User user = userRepo.findById(userId);
+
+        if (user == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Usuário não encontrado!")).build();
+
+        PostEntity newPost = new PostEntity(user, dto.getText());
+        repo.persist(newPost);
+
+        return Response.created(UriBuilder.fromResource(UserResource.class).path(userId.toString()).path("posts").path(newPost.getId().toString()).build()).build();
+    }
+
+    public Response posts(Long userId) {
+        User user = userRepo.findById(userId);
+
+        if (user == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Usuário não encontrado!")).build();
+
+        return Response.ok(repo.find("user", Sort.by("dateTime", Sort.Direction.Descending), user)
+                .stream()
+                .map(PostDtoOut::fromEntity)
+                .collect(Collectors.toList())).build();
+    }
+
+    public Response update(Long userId, Long postId, PostDtoIn dto) {
+        Set<ConstraintViolation<PostDtoIn>> violations = validator.validate(dto);
+
+        if (!violations.isEmpty())
+            return ResponseError
+                    .createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
+
+        User user = userRepo.findById(userId);
+        if (user == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Usuário não encontrado!")).build();
+
+        PostEntity postEntity = repo.find("user = :user and id = :id", Parameters.with("user", user).and("id", postId)).firstResult();
+        if (postEntity == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Post não encontrado!")).build();
+
+        postEntity.setText(dto.getText());
+
+        return Response.status(Response.Status.OK.getStatusCode()).entity(new ResponseMsg("Post atualizado!")).build();
+    }
+
+    public Response delete(Long userId, Long postId) {
+        User user = userRepo.findById(userId);
+        if (user == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Usuário não encontrado!")).build();
+
+        PostEntity postEntity = repo.find("user = :user and id = :id", Parameters.with("user", user).and("id", postId)).firstResult();
+        if (postEntity == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Post não encontrado!")).build();
+
+        repo.delete(postEntity);
+
+        return Response.status(Response.Status.NO_CONTENT.getStatusCode()).entity(new ResponseMsg("Post deletado!")).build();
+    }
+}
